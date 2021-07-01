f8a761cb0141a3aaa9b6d976aa95d3f4d8e65ae3
Greg Sronce <greg.sronce@gmail.com>
2021-06-30 21:30:20 -0400

    Able to display the first card in deck

--- static/js/peekaboo.js
+++ static/js/peekaboo.js
@@ -1,5 +1,13 @@
 console.log('peekaboo')
 
 document.querySelector('.start-deck').addEventListener('click',() => {
-    console.log('function working?')
+    const display = document.getElementById('1');
+    // console.log(display)
+    display.classList.replace("display-none", "active")
+    const button = document.getElementById('start-deck-button');
+        if (button.innerText =='Start Deck') { 
+            button.innerText = 'Next Card'
+        } 
 })
+
+
